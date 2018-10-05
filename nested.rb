ddce1218fae67103c7294b2420401b18fa191487
Yu Li <yli0607x@gmail.com>
2018-10-05 15:52:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -81,7 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+    :languages => ["LISP, C"]}
+		programmer_hash
 
 end
 
